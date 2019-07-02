fece72772e8be8543d985b1029ec788471d7f025
Ap in git <secretuser@gmail.com>
2019-07-02 14:58:49 +0545

    Navbar made without responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-document.getElementById('nav-dropdown-trigger').addEventListener('click',()=>{
-    document.getElementById('nav-dropdown').classList.toggle('hidden');
+let nav_drop_down_trigger = document.getElementsByClassName('dropdown-trigger');
+
+Array.from(nav_drop_down_trigger).forEach((e) => {
+    let toggle_name = e.getAttribute('data-toggle');
+    if (document.getElementById(toggle_name)) {
+        e.addEventListener('click', () => document.getElementById(toggle_name).classList.toggle('hidden'));
+    }
 });
+
+
+window.onclick = function (e) {
+    if (!e.target.matches('.dropdown-trigger')) {
+        let elements = document.getElementsByClassName('dropdown-trigger');
+        for (let el of elements) {
+            hideOutsideDivClick(el.getAttribute('data-toggle'));
+        }
+    } else {
+        let target_el = e.target.getAttribute('data-toggle');
+        let elements = document.getElementsByClassName('dropdown-trigger');
+        for (let el of elements) {
+            if (target_el !== el.getAttribute('data-toggle')) {
+                hideOutsideDivClick(el.getAttribute('data-toggle'));
+            }
+        }
+    }
+};
+
+function hideOutsideDivClick(id) {
+    let element = document.getElementById(id);
+    if (!element.classList.contains('hidden')) {
+        element.classList.add('hidden');
+    }
+}
